ebac9c32837d53b8533bfb333868f9eec2543ea9
kaseywright <kasey.wright@biblionexus.org>
2025-03-04 13:26:31 -0500

    rename tsa2025 to aquifer-images

--- packages/aquifer-images/src/browser/aquifer-images-contribution.ts
+++ packages/aquifer-images/src/browser/aquifer-images-contribution.ts
@@ -1,7 +1,7 @@
 import { injectable } from '@theia/core/shared/inversify';
 import { AbstractViewContribution } from '@theia/core/lib/browser';
 import { Command, CommandRegistry } from '@theia/core/lib/common/command';
-import { AquiferWidget } from './tsa2025-widget';
+import { AquiferWidget } from './aquifer-images-widget';
 import { MenuModelRegistry } from '@theia/core';
 
 export const AquiferCommand: Command = { id: 'aquifer:command' };
@@ -12,7 +12,7 @@ export class AquiferContribution extends AbstractViewContribution<AquiferWidget>
         super({
             widgetId: AquiferWidget.ID,
             widgetName: AquiferWidget.LABEL,
-            defaultWidgetOptions: { area: 'left' },
+            defaultWidgetOptions: { area: 'right' },
             toggleCommandId: AquiferCommand.id
         });
     }
